cecbc154188a85e48c359b54fa20f85616af3ab2
fuskovic <fhuskovic92@gmail.com>
2025-01-11 21:16:05 +0100

    implement commands

--- cmd/sym/main.go
+++ cmd/sym/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+
 	"github.com/fuskovic/sym"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -55,9 +61,27 @@ var encryptCmd = &cobra.Command{
 	Use:     "encrypt",
 	Short:   "encrypt text or files",
 	Example: `TODO`,
-	Args:    cobra.NoArgs,
+	Args:    cobra.ArbitraryArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		k := getKey(cmd)
+		w := getTargetWriter(cmd)
+		if inFile != "" {
+			if outFile == "" {
+				outFile = getDefaultOutFile(cmd)
+				fmt.Printf("defaulting to out-file: %s", outFile)
+			}
+			if err := sym.EncryptFile(k, inFile, outFile); err != nil {
+				usageError(cmd, err)
+			}
+			return
+		}
+		if len(args) > 0 {
+			plainText, err := sym.EncryptString(k, args[0])
+			if err != nil {
+				usageError(cmd, err)
+			}
+			w.Write([]byte(plainText))
+		}
 	},
 }
 
@@ -65,9 +89,27 @@ var decryptCmd = &cobra.Command{
 	Use:     "decrypt",
 	Short:   "decrypt text or files",
 	Example: `TODO`,
-	Args:    cobra.NoArgs,
+	Args:    cobra.ArbitraryArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		k := getKey(cmd)
+		w := getTargetWriter(cmd)
+		if inFile != "" {
+			if outFile == "" {
+				outFile = getDefaultOutFile(cmd)
+				fmt.Printf("defaulting to out-file: %s", outFile)
+			}
+			if err := sym.DecryptFile(k, inFile, outFile); err != nil {
+				usageError(cmd, err)
+			}
+			return
+		}
+		if len(args) > 0 {
+			plainText, err := sym.DecryptString(k, args[0])
+			if err != nil {
+				usageError(cmd, err)
+			}
+			w.Write([]byte(plainText))
+		}
 	},
 }
 
@@ -77,5 +119,67 @@ var keyGenCmd = &cobra.Command{
 	Example: `TODO`,
 	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
+		key, err := sym.KeyGen(size)
+		if err != nil {
+			usageErrorf(cmd, "failed to generate key: %w", err)
+		}
+		getTargetWriter(cmd).Write([]byte(key))
 	},
+}
+
+func usageError(cmd *cobra.Command, errMsg any) {
+	var msg string
+	switch t := errMsg.(type) {
+	case string:
+		msg = t
+	case error:
+		msg = t.Error()
+	default:
+		msg = fmt.Sprintf("%v", msg)
+	}
+	cmd.Usage()
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func usageErrorf(cmd *cobra.Command, format string, args ...any) {
+	usageError(cmd, fmt.Errorf(format, args...))
+}
+
+func getTargetWriter(cmd *cobra.Command) io.Writer {
+	var w io.Writer = os.Stdout
+	var err error
+	if outFile != "" {
+		w, err = os.Create(outFile)
+		if err != nil {
+			usageErrorf(cmd, "failed to open %s: %w", outFile, err)
+		}
+	}
+	return w
+}
+
+func getKey(cmd *cobra.Command) string {
+	if key == "" && keyFile == "" {
+		usageError(cmd, "must provide key or key-file")
+	}
+	if key != "" && keyFile != "" {
+		usageError(cmd, "key and keyfile provided; must be either or")
+	}
+
+	var err error
+	if keyFile != "" {
+		key, err = sym.KeyFromFilePath(keyFile)
+		if err != nil {
+			usageErrorf(cmd, "failed to get key from file: %w", err)
+		}
+	}
+	return key
+}
+
+func getDefaultOutFile(cmd *cobra.Command) string {
+	base := strings.TrimSuffix(inFile, path.Ext(inFile))
+	if cmd.Use == "encrypt" {
+		return base + ".enc"
+	}
+	return base + ".txt"
 }
